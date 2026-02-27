9c290b16ee0546e5adb84ae47ef0aa0eb6b110e9
Shubham <shubham555bgp@gmail.com>
2026-02-28 00:04:27 +0530

    created Frontend folder

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,15 @@
+// import './App.css'
+import './index.css'
+import GroupPage from './pages/GroupPage.jsx'
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl font-bold text-blue-600">
+      Tailwind Working 
+    </h1>
+    <GroupPage/>
+    </>
+  )
+}
+
+export default App
---
+++ Frontend/src/pages/GroupPage.jsx
@@ -0,0 +1,9 @@
+export default function GroupPage() {
+    return (
+        <>
+            <div className="card box-border size-32 border-4 p-4">
+                hi
+            </div>
+        </>
+    )
+}
